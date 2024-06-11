9bb98eb5e007e9cf2ccea9ca516052e031181180
6luv <jmj00005@naver.com>
2024-06-11 16:35:55 +0900

    style: font

--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "WavvePADO",
+    body: "WavvePADO",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
