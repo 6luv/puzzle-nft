723eadaa0d45eb8cdd7fb0cf260610f58c397673
6luv <jmj00005@naver.com>
2024-06-12 16:46:40 +0900

    feat: my page login

--- vite/src/pages/My.tsx
+++ vite/src/pages/My.tsx
@@ -1,4 +1,4 @@
-import { Flex, Grid } from "@chakra-ui/react";
+import { Button, Flex, Grid, Text } from "@chakra-ui/react";
 import { FC, useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import { OutletContext } from "../components/Layout";
@@ -6,7 +6,7 @@ import NftCard from "../components/NftCard";
 
 const My: FC = () => {
   const [mintedList, setMintedList] = useState<number[]>([]);
-  const { signer, mintContract } = useOutletContext<OutletContext>();
+  const { signer, setSigner, mintContract } = useOutletContext<OutletContext>();
 
   const getCheckNfts = async () => {
     try {
@@ -27,18 +27,46 @@ const My: FC = () => {
   useEffect(() => console.log(mintedList), [mintedList]);
 
   return (
-    <Flex flexDir="column" w="100%" p={8}>
-      <Grid
-        templateColumns={["repeat(2, 1fr)", "repeat(2, 1fr)", "repeat(4, 1fr)"]}
-        gap={8}
-        justifyItems="center"
-      >
-        {mintedList.map((v, i) => {
-          if (v > 0) {
-            return <NftCard key={i} tokenId={i + 1} amount={v} />;
-          }
-        })}
-      </Grid>
+    <Flex
+      flexDir="column"
+      w="100%"
+      alignItems="center"
+      justifyContent="center"
+      border="2px"
+      borderColor="gray.200"
+      boxShadow="md"
+      rounded="lg"
+    >
+      {signer ? (
+        <Flex flexDir="column" w="100%" p={8}>
+          <Grid
+            templateColumns={[
+              "repeat(2, 1fr)",
+              "repeat(2, 1fr)",
+              "repeat(4, 1fr)",
+            ]}
+            gap={8}
+            justifyItems="center"
+          >
+            {mintedList.map((v, i) => {
+              if (v > 0) {
+                return <NftCard key={i} tokenId={i + 1} amount={v} />;
+              }
+            })}
+          </Grid>
+        </Flex>
+      ) : (
+        <Flex flexDir="column" gap={[2, 2, 4]} alignItems="center">
+          <Text fontSize={[20, 20, 28]}>로그인 후 이용해 주세요!</Text>
+          <Button
+            w="fit-content"
+            textColor="blue.500"
+            onClick={() => useMetamaskLogin(setSigner)}
+          >
+            Login
+          </Button>
+        </Flex>
+      )}
     </Flex>
   );
 };
