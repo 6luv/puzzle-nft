2296462a4c6321ffdb6cb91b11e26ac3be906b98
6luv <jmj00005@naver.com>
2024-06-12 14:34:53 +0900

    feat: nft mint button

--- vite/src/pages/Mint.tsx
+++ vite/src/pages/Mint.tsx
@@ -1,11 +1,45 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
-import { FC } from "react";
+import {
+  Button,
+  Flex,
+  Image,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Text,
+} from "@chakra-ui/react";
+import { FC, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import { OutletContext } from "../components/Layout";
 import { useMetamaskLogin } from "../lib";
+import axios from "axios";
 
 const Mint: FC = () => {
-  const { signer, setSigner } = useOutletContext<OutletContext>();
+  const [tokenId, setTokenId] = useState<number>(1);
+  const [amount, setAmount] = useState<number>(1);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { signer, setSigner, mintContract } = useOutletContext<OutletContext>();
+
+  const onClickMintNft = async () => {
+    try {
+      setIsLoading(true);
+      if (!mintContract || !tokenId || !amount) return;
+
+      const response = await mintContract?.mintNft(tokenId, amount);
+      await response.wait();
+
+      const axiosResponse = await axios.get<INftMetadata>(
+        `${import.meta.env.VITE_METADATA_URI}/${tokenId}.json`
+      );
+      console.log(axiosResponse);
+
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Flex
@@ -19,8 +53,70 @@ const Mint: FC = () => {
       rounded="lg"
     >
       {signer ? (
-        <Flex flexDir="column" gap={[4, 4, 8]}>
-          {signer.address}
+        <Flex flexDir="column" alignItems="center" gap={4} px={4}>
+          <Image
+            w={[60, 60, 80]}
+            mb={4}
+            border="2px"
+            borderColor="gray.200"
+            boxShadow="xl"
+            rounded="lg"
+            src={`/images/puzzle/${tokenId}.png`}
+            alt={`Save the SEA #${tokenId}`}
+          />
+
+          <Flex alignItems="center" gap={6}>
+            <Text fontWeight="semibold" fontSize="lg">
+              NFT ID
+            </Text>
+            <NumberInput
+              size="lg"
+              value={tokenId}
+              onChange={(v) => setTokenId(Number(v))}
+              defaultValue={1}
+              min={1}
+              max={16}
+              w={[180, 180, 200]}
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </Flex>
+          <Flex alignItems="center" gap={4}>
+            <Text fontWeight="semibold" fontSize="lg">
+              Amount
+            </Text>
+            <NumberInput
+              size="lg"
+              value={amount}
+              onChange={(v) => setAmount(Number(v))}
+              defaultValue={0}
+              min={0}
+              max={100}
+              w={[180, 180, 200]}
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </Flex>
+          <Button
+            w="full"
+            textColor="blue.500"
+            bgColor="white"
+            border="1px"
+            onClick={onClickMintNft}
+            isDisabled={isLoading}
+            isLoading={isLoading}
+            loadingText="Minting"
+          >
+            Minting
+          </Button>
         </Flex>
       ) : (
         <Flex flexDir="column" gap={[2, 2, 4]} alignItems="center">
